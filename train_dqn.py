84d4eb4eef94a97935de927c1cb280222dc79fb5
breadbread1984 <breadbread1984@163.com>
2019-02-27 09:38:40 +0800

    new file:   train_dqn.py 	modified:   train_vpg.py

--- train_dqn.py
+++ train_dqn.py
@@ -7,12 +7,12 @@ import tensorflow as tf;
 from atari_py import ALEInterface;
 from atari_py import get_game_path;
 
-class PolicyModel(tf.keras.Model):
+class QNet(tf.keras.Model):
 
     def __init__(self, legal_actions = None):
 
         assert type(legal_actions) is np.ndarray and len(legal_actions) > 0;
-        super(PolicyModel,self).__init__();
+        super(QNet,self).__init__();
         self.conv1 = tf.keras.layers.Conv2D(64, kernel_size = [3,3], padding = 'same');
         self.relu1 = tf.keras.layers.ReLU();
         self.maxpool1 = tf.keras.layers.MaxPool2D(pool_size = [2,2], strides = [2,2], padding = 'valid');
@@ -25,11 +25,8 @@ class PolicyModel(tf.keras.Model):
         self.dense4 = tf.keras.layers.Dense(200);
         self.dropout4 = tf.keras.layers.Dropout(0.5);
         self.relu4 = tf.keras.layers.ReLU();
-        # output P(a|s)
+        # output Q(s,a)
         self.dense5 = tf.keras.layers.Dense(legal_actions.shape[0], activation = tf.math.sigmoid);
-        self.exp = tf.keras.layers.Lambda(lambda x: tf.math.exp(x));
-        # output V(s)
-        self.dense6 = tf.keras.layers.Dense(1, activation = tf.math.sigmoid);
         
     def call(self, input):
         
@@ -45,24 +42,22 @@ class PolicyModel(tf.keras.Model):
         result = self.dense4(result);
         result = self.dropout4(result);
         result = self.relu4(result);
-        logP = self.dense5(result);
-        P = self.exp(logP);
-        V = self.dense6(result);
-        return V, P;
+        Q = self.dense5(result);
+        return Q;
 
-class VPG(object):
+class DQN(object):
     
     def __init__(self):
         
         self.ale = ALEInterface();
         self.ale.loadROM(get_game_path('boxing'));
         self.legal_actions = self.ale.getMinimalActionSet();
-        self.policyModel = PolicyModel(self.legal_actions);
+        self.qnet = QNet(self.legal_actions);
         #load model
-        if True == os.path.exists('model'): self.policyModel.load_weights('./model/vpg_model');
+        if True == os.path.exists('model'): self.qnet.load_weights('./model/dqn_model');
         self.status_size_ = 4
         self.gamma_ = 1; #the reward it too small
-        
+
     def status2tensor(self,status):
         
         status = tf.convert_to_tensor(status, dtype = tf.float32);
@@ -87,63 +82,54 @@ class VPG(object):
             cv2.waitKey(10);
             # choose action 
             input = self.status2tensor(status);
-            V, P = self.policyModel(input);
-            action_index = tf.random.categorical(P,1);
+            Qt = self.qnet(input);
+            action_index = tf.random.categorical(tf.math.exp(Qt),1);
             reward = self.ale.act(self.legal_actions[action_index]);
             status.append(cv2.resize(self.ale.getScreenGrayscale(),(160,160)));
             trajectory.append((status[0:self.status_size_],action_index,reward,status[1:]));
             status = status[1:];
-        total_reward = 0;
-        for status in reversed(trajectory):
-            total_reward = status[2] + self.gamma_ * total_reward;
-        return trajectory, total_reward;
+        return trajectory;
     
     def train(self, loop_time = 1000):
         
         optimizer = tf.keras.optimizers.Adam(1e-3);
         # setup checkpoint and log utils
-        checkpoint = tf.train.Checkpoint(model = self.policyModel, optimizer = optimizer, optimizer_step = optimizer.iterations);
-        checkpoint.restore(tf.train.latest_checkpoint('checkpoints'));
-        log = tf.summary.create_file_writer('checkpoints');
+        checkpoint = tf.train.Checkpoint(model = self.qnet, optimizer = optimizer, optimizer_step = optimizer.iterations);
+        checkpoint.restore(tf.train.latest_checkpoint('checkpoints_dqn'));
+        log = tf.summary.create_file_writer('checkpoints_dqn');
         for i in range(loop_time):
-            trajectory, total_reward = self.PlayOneEpisode();
-            avg_policy_loss = tf.keras.metrics.Mean(name = 'policy loss', dtype = tf.float32);
-            avg_value_loss = tf.keras.metrics.Mean(name = 'value loss', dtype = tf.float32);
+            trajectory = self.PlayOneEpisode();
+            avg_loss = tf.keras.metrics.Mean(name = 'loss', dtype = tf.float32);
             for status in trajectory:
                 # policy loss
                 with tf.GradientTape() as tape:
-                    Vt, Pt = self.policyModel(self.status2tensor(status[0]));
-                    Vtp1, Ptp1 = self.policyModel(self.status2tensor(status[3]));
+                    Qt = self.qnet(self.status2tensor(status[0]));
+                    Qtp1 = self.qnet(self.status2tensor(status[3]));
                     action_mask = tf.one_hot(status[1],len(self.legal_actions));
-                    probs = tf.math.reduce_sum(action_mask * Pt, axis = 1);
-                    advantage = -Vt + status[2] + self.gamma_ * Vtp1;
-                    policy_loss = -tf.math.reduce_mean(probs * advantage);
-                    value_loss = tf.math.squared_difference(Vt, total_reward);
-                    loss = policy_loss + value_loss;
-                    avg_policy_loss.update_state(policy_loss);
-                    avg_value_loss.update_state(value_loss);
+                    qt = tf.math.reduce_sum(action_mask * Qt, axis = 1);
+                    qtp1 = tf.math.reduce_max(Qtp1, axis = 1);
+                    value = status[2] + self.gamma_ * qtp1;
+                    loss = tf.math.squared_difference(qt, value);
+                    avg_loss.update_state(loss);
                 # write loss to summary
                 if tf.equal(optimizer.iterations % 100, 0):
                     with log.as_default():
-                        tf.summary.scalar('policy loss',avg_policy_loss.result(), step = optimizer.iterations);
-                        tf.summary.scalar('value loss',avg_value_loss.result(), step = optimizer.iterations);
-                    avg_policy_loss.reset_states();
-                    avg_value_loss.reset_states();
+                        tf.summary.scalar('loss',avg_loss.result(), step = optimizer.iterations);
+                    avg_loss.reset_states();
                 # train policy and value
-                grads = tape.gradient(loss,self.policyModel.variables);
-                optimizer.apply_gradients(zip(grads,self.policyModel.variables));
+                grads = tape.gradient(loss,self.qnet.variables);
+                optimizer.apply_gradients(zip(grads,self.qnet.variables));
             # save model every episode
-            checkpoint.save(os.path.join('checkpoints','ckpt'));
+            checkpoint.save(os.path.join('checkpoints_dqn','ckpt'));
         # save final model
         if False == os.path.exists('model'): os.mkdir('model');
-        #tf.saved_model.save(self.policyModel,'./model/vpg_model');
-        self.policyModel.save_weights('./model/vpg_model');
+        #tf.saved_model.save(self.qnet,'./model/vpg_model');
+        self.qnet.save_weights('./model/dqn_model');
 
 def main():
 
-    vpg = VPG();
-    vpg.train(1000);
-    #vpg.PlayOneEpisode();
+    dqn = DQN();
+    dqn.train(1000);
 
 if __name__ == "__main__":
 
